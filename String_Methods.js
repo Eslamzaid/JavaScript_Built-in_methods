a05dc5b6aacdd767755b58c62ccf1e40963c61f5
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-01-01 11:19:57 +0300

    String Methods

---
+++ String_Methods.js
@@ -0,0 +1,25 @@
+//! charAt()
+
+var hello = "Hello friend!"
+console.log(hello.charAt(1))
+
+//? return the char.. at the specified 'Index'
+//* Will return 'e' 0=H , 1=e
+
+
+//! charCodeAt()
+
+var hello = "Hello world!"
+console.log(hello.charCodeAt(0))
+
+//? return the unicode of the char at the specified index in a string.
+//* Will return '72
+
+//! concat()
+
+var hello = "Hello"
+var world = " World"
+console.log(hello.concat(world))
+
+//? to join two or more strings together
+//* Will return "Hello World"
