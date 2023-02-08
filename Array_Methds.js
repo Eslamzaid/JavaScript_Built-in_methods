59a9e06192d1f8be7dfea270484b3fe5cc35fa0c
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-02-08 10:02:28 +0300

    Array_Methods

--- Array_Methds.js
+++ Array_Methds.js
@@ -1,12 +1,12 @@
 //! constructor 
-var fruitCons = ['Banana','Apple','Watermelon']
+var fruitCons = ['Banana', 'Apple', 'Watermelon']
 console.log(fruitCons)
 //? Returns the constructor function -for an object
 //* Will return = [ 'Banana', 'Apple', 'Watermelon' ]
 
 
 //! length
-var fruitLeng = ['Banana','Apple','Watermelon']
+var fruitLeng = ['Banana', 'Apple', 'Watermelon']
 console.log(fruitLeng.length)
 //* Will return 3
 fruitLeng.length = 1
@@ -20,8 +20,25 @@ console.log(fruitLeng)
 
 
 //! concat
-var fruitCon1 = ['Messi','mbappe']
+var fruitCon1 = ['Messi', 'mbappe']
 var fruitCon2 = ['neymar']
 const addThem = fruitCon1.concat(fruitCon2)
 console.log(addThem)
 //? is used to join two or more arrays , it does not change the array, it create a new one.
+
+
+//! copyWithIn()
+console.log([1, 2, 3, 4, 5].copyWithin(-2));
+// [1, 2, 3, 1, 2]
+
+console.log([1, 2, 3, 4, 5].copyWithin(0, 3));
+// [4, 5, 3, 4, 5]
+
+console.log([1, 2, 3, 4, 5].copyWithin(0, 3, 4));
+// [4, 2, 3, 4, 5]
+
+console.log([1, 2, 3, 4, 5].copyWithin(-2, -3, -1));
+// [1, 2, 3, 3, 4]
+//?copies array elements to another position in the array, overwriting the existing values.
+
+
