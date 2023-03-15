04d063a0dca3e38505801f0e2e71de22a8a81443
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-03-15 10:21:11 +0300

    Array_Methods

--- Array_Methods.js
+++ Array_Methods.js
@@ -257,5 +257,4 @@ console.log(someNum);
 //? It returns true if, in the array, it finds an element for which the provided function returns true; otherwise it returns false.
 //? It doesn't modify the array.
 
-
 // test
